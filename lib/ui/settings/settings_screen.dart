5e6db136abf6f084e5a4726358e4a61030e84bf9
Jean Vallon <jvallon44@gmail.com>
2025-11-20 20:51:55 +0100

    clean(settings): add back button and fix margins

--- lib/ui/settings/settings_screen.dart
+++ lib/ui/settings/settings_screen.dart
@@ -1,61 +1,65 @@
 import 'package:betclictactoe/theme/colors.dart';
-import 'package:betclictactoe/ui/shared/widgets/app_button.dart';
 import 'package:flutter/material.dart' hide Colors;
-import 'package:go_router/go_router.dart';
 
 import 'custom_name_dialog.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
-  static const _gap = SizedBox(height: 60);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.backgroundSettings,
-      body: SingleChildScrollView(
-        child: Column(
-          spacing: 60,
-          children: [
-            _gap,
-            const Text(
-              'Settings',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'Permanent Marker',
-                fontSize: 55,
-                height: 1,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: Column(
+            spacing: 60,
+            children: [
+              Row(
+                children: [
+                  BackButton(
+                    style: ButtonStyle(iconSize: WidgetStateProperty.all(50)),
+                  ),
+                  Expanded(
+                    child: const Text(
+                      'Settings',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Permanent Marker',
+                        fontSize: 55,
+                        height: 1,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            _gap,
-            const _NameChangeLine('Name'),
-            _SettingsLine(
-              'Sound FX',
-              // TODO: : Get value from notifier
-              Icon(true ? Icons.graphic_eq : Icons.volume_off),
-              onSelected: () {
-                // TODO: Call notifier
-              },
-            ),
-            _SettingsLine(
-              'Music',
-              // TODO: : Get value from notifier
-              Icon(true ? Icons.music_note : Icons.music_off),
-              onSelected: () {
-                // TODO: Call notifier
-              },
-            ),
-            _gap,
-            const Text('Music by Mr Smith'),
-            _gap,
-            AppButton(
-              onPressed: () {
-                context.pop();
-              },
-              child: const Text('Back'),
-            ),
-          ],
+              Expanded(
+                child: ListView(
+                  children: [
+                    const _NameChangeLine('Name'),
+                    _SettingsLine(
+                      'Sound FX',
+                      // TODO: : Get value from notifier
+                      Icon(true ? Icons.graphic_eq : Icons.volume_off),
+                      onSelected: () {
+                        // TODO: Call notifier
+                      },
+                    ),
+                    _SettingsLine(
+                      'Music',
+                      // TODO: : Get value from notifier
+                      Icon(true ? Icons.music_note : Icons.music_off),
+                      onSelected: () {
+                        // TODO: Call notifier
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const Text('Music by Mr Smith'),
+            ],
+          ),
         ),
       ),
     );
