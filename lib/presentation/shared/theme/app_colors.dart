fb82c690430d5871f5cf570ba75794e602c64d6a
Jean Vallon <jvallon44@gmail.com>
2025-11-21 16:55:05 +0100

    feat(settings): enhance background color settings

--- lib/presentation/shared/theme/app_colors.dart
+++ lib/presentation/shared/theme/app_colors.dart
@@ -5,7 +5,8 @@ class AppColors {
   static Color get background => const Color(0xFFEFF0F1);
 
   static Color get backgroundHome => primary;
-  static Color get backgroundSettings => const Color(0xffbfc8e3);
+  static Color get backgroundSettings =>
+      const Color.fromARGB(255, 248, 163, 155);
   static Color get darkPen => const Color(0xFF0050bc);
   static Color get ink => const Color(0xee352b42);
 }
